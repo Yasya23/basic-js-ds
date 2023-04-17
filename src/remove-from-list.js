228d9ba6dfce80832546a81b0ebd310513e4ed3e
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-04-17 10:55:08 +0300

    feat: remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l === null) return null;
+
+  while (l.value === k) {
+    l = l.next;
+    if (l === null) return null;
+  }
+
+  let currentElement = l.next;
+  let prevElement = l;
+
+  while (currentElement !== null) {
+    currentElement.value === k
+      ? (prevElement.next = currentElement.next)
+      : (prevElement = currentElement);
+    currentElement = currentElement.next;
+  }
+
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
