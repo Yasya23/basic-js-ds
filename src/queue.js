cfe4d1a70da9ff59763a81b299615421a48f44c1
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-04-17 13:01:45 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    !this.head
+      ? (this.head = this.length = node)
+      : (this.length = this.length.next = node);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) return null;
+
+    const value = this.head.value;
+    this.head = this.head.next;
+
+    if (!this.head) this.length = 0;
+
+    return value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
